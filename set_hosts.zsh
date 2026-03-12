ddcb8c5840bfb509810583416c8699822a5246f6
Richard <richard.zen.liew@gmail.com>
2026-03-12 14:57:47 +0800

    Implement hostname assignment functions in set_hosts.zsh
    Add functions to assign and set hostnames on macOS.

---
+++ set_hosts.zsh
@@ -0,0 +1,24 @@
+# Hosts:
+function assign_hosts() {
+    local -r computer_name = "$1"
+    local -r local_hostname = "$2"
+    local -r hostname = "$3"
+
+    sudo scutil --set ComputerName "${computer_name}"
+    sudo scutil --set LocalHostName "${local_hostname}"
+    sudo scutil --set HostName "${hostname}"
+
+    sudo dscacheutil -flushcache
+    sudo killall -HUP mDNSResponder
+}
+
+
+function set_hosts() {
+    assign_hosts \
+        "Richard's MacBook Pro" \
+        "RichardMacBookPro" \
+        "anebit.cn"
+}
+
+
+set_hosts
