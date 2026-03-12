ba3551c4caee804c33025e9c03010a093fb48a03
Richard <richard.zen.liew@gmail.com>
2026-03-12 14:59:58 +0800

    Fix formatting issues in set_hosts.zsh

--- set_hosts.zsh
+++ set_hosts.zsh
@@ -1,23 +1,23 @@
 # Hosts:
 function assign_hosts() {
-    local -r computer_name = "$1"
-    local -r local_hostname = "$2"
-    local -r hostname = "$3"
+  local -r computer_name = "$1"
+  local -r local_hostname = "$2"
+  local -r hostname = "$3"
 
-    sudo scutil --set ComputerName "${computer_name}"
-    sudo scutil --set LocalHostName "${local_hostname}"
-    sudo scutil --set HostName "${hostname}"
+  sudo scutil --set ComputerName "${computer_name}"
+  sudo scutil --set LocalHostName "${local_hostname}"
+  sudo scutil --set HostName "${hostname}"
 
-    sudo dscacheutil -flushcache
-    sudo killall -HUP mDNSResponder
+  sudo dscacheutil -flushcache
+  sudo killall -HUP mDNSResponder
 }
 
 
 function set_hosts() {
-    assign_hosts \
-        "Richard's MacBook Pro" \
-        "RichardMacBookPro" \
-        "anebit.cn"
+  assign_hosts \
+    "Richard's MacBook Pro" \
+    "RichardMacBookPro" \
+    "anebit.cn"
 }
 
 
